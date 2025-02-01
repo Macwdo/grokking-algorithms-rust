6924e51d1d73e590541663cdd23d26dc9e7c5955
Danilo Macedo Azevedo Fernandes <danilo.macedofernandes@hotmail.com>
2025-02-01 03:40:08 -0300

    :sparkles: adding selection sort

--- src/chapter_2.rs
+++ src/chapter_2.rs
@@ -1,16 +1,18 @@
 pub fn main(args: Vec<String>) {
     let size = args[1].parse().unwrap();
 
-    let arr = helpers::random_arr(size);
-    sorts::selection_sort(1, &arr);
+    let mut temp = helpers::random_arr(size);
+    let arr = temp.as_mut_slice();
+
+    sorts::selection_sort(arr);
 }
 
 mod helpers {
     use rand::Rng;
 
     pub fn random_arr(size: u128) -> Vec<u128> {
-        let mut rng = rand::rng();
         let mut arr = Vec::with_capacity(size as usize);
+        let mut rng = rand::rng();
 
         for _ in 0..size {
             let a = rng.random_range(0..100);
@@ -18,15 +20,40 @@ mod helpers {
         }
         return arr;
     }
+
+    pub fn show_values(array: &[u128]) {
+        let n = array.len();
+        let mut str_buffer = "[".to_string();
+
+        for (i, value) in array.iter().enumerate() {
+            if i < n - 1 {
+                str_buffer += &format!("{}, ", value);
+            } else {
+                str_buffer += &format!("{}]", value);
+            }
+        }
+
+        println!("{}", str_buffer);
+    }
 }
 
 mod sorts {
-    pub fn selection_sort(target: u128, array: &[u128]) {
-        let n = array.len() - 1;
+    use super::helpers;
+
+    pub fn selection_sort(array: &mut [u128]) {
+        let n = array.len();
+
         for i in 0..n {
+            let mut min = i;
             for j in i..n {
-                println!("{} {}", i, j)
+                if array[min] > array[j] {
+                    min = j;
+                }
             }
+            let temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+            helpers::show_values(array);
         }
     }
 }
